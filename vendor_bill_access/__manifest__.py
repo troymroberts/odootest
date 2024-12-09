4ff035381a857c2511c558b977a5d7f5f10b3306
troymroberts <troy.roberts@paxgenesis.com>
2024-12-09 17:37:05 -0400

    add vendor bill access

---
+++ vendor_bill_access/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Vendor Bill Access',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Custom access rights for vendor bill upload',
+    'description': """
+        This module provides restricted access for users to only upload and manage vendor bills
+        without access to other accounting features.
+    """,
+    'depends': ['base', 'account', 'account_edi'],  # Added account_edi
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/vendor_bill_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'vendor_bill_access/static/**/*',
+        ],
+    },
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
